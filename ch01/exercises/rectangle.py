d36a79f7ff7380c3b3a75779c7ef34555c8f15c1
Anthony Parisi <anthonyparisi@Anthonys-MacBook-Air.local>
2023-09-08 15:48:21 -0400

    yippee

---
+++ ch01/exercises/rectangle.py
@@ -0,0 +1,7 @@
+shape = "dodecagon"
+sides = 12
+int = internal_angle
+
+internal_angle = ((sides-2)*180)/sides
+
+print(inernal_angle)
--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,22 @@
+import random
 
+#Part A
+weeks = 16
+print(weeks,type(weeks))
+classes = 5
+print(classes,type(classes))
+tuition = 15000
+print(tuition,type(tuition))
+classes_per_week = 11
+print(classes_per_week,type(classes_per_week))
+cost_per_week = ((tuition/classes)/weeks)
+print(cost_per_week,type(cost_per_week))
+print("cost per week:", cost_per_week)
+cost_per_class = (cost_per_week/classes_per_week)
+print(cost_per_class,type(cost_per_class))
+print("cost per class:",cost_per_class)
+
+#part B
+mylist = ["Hello",4.5,13,"What?","Goodbye"]
+random_choice = random.choice(mylist)
+print(random_choice)
