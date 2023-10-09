d166f589fbe127d15d73602f23e255ed1893046e
Anthony Parisi <anthonyparisi@Anthonys-MacBook-Air.local>
2023-10-09 17:45:32 -0400

    nice

--- ch04/exercises/guessagain.py
+++ ch04/exercises/guessagain.py
@@ -1,9 +1,13 @@
 import random
+import math
 
-number = random.randint(1,10)
+number = random.randint(1,1000)
+guess = 0
+number_guesses = 0
 
-for i in range(3):
+while guess != number:
     guess = int(input("please enter a guess for the number: "))
+    number_guesses += 1
     if number == guess:
         print("That's right!")
         break
@@ -12,3 +16,6 @@ for i in range(3):
     elif number > guess:
         print("too low")
         
+print("It took you ", number_guesses, " guesses to get it right, the number was", number)
+print("It should take you at most", 9, "guesses")
+
