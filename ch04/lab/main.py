1d738433bb16b314aba5a4084861d7aef8ed431b
Anthony <anthonyp1455@gmail.com>
2023-10-13 22:50:20 -0400

    lab

--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -20,93 +20,56 @@ pygame.draw.line(screen,"black",(0,250),(500,250))
 pygame.display.flip()
 pygame.time.wait(1000)
 
-    
-for i in range(10):
-    x1 = random.randrange(screen_width)
-    y1 = random.randrange(screen_height)
-    x2 = random.randrange(screen_width)
-    y2 = random.randrange(screen_height)
 
-    distance_from_center1 = math.hypot(x1-250, y1-250)
-    distance_from_center2 = math.hypot(x2-250, y2-250)
-    is_in_circle1 = distance_from_center1 <= screen_width/2
-    is_in_circle2 = distance_from_center2 <= screen_width/2
 
-    if is_in_circle1 == True:
-        pygame.draw.circle(screen, "yellow", (x1,y1),5)
-        pygame.display.flip()
-        player1_score = player1_score + 1
-    else:
-        pygame.draw.circle(screen, "blue", (x1,y1),5)
-        pygame.display.flip()
-    if is_in_circle2 == True:
-        pygame.draw.circle(screen, "pink", (x2,y2),5)
-        pygame.display.flip()
-        pygame.time.wait(500)
-        player2_score = player2_score + 1
-    else:
-        pygame.draw.circle(screen, "purple", (x2,y2),5)
-        pygame.display.flip()
-        pygame.time.wait(500)
+while True:
+    for event in pygame.event.get():
+        print(event)
     
-if player1_score == player2_score:
-    text = pygame.font.render("asdasda",True, "white")
-    screen.blit(text, (250,250))
-    
-elif player1_score > player2_score:
-    text = pygame.font.render("adsasd",True, "white")
-    screen.blit(text, (250,250))
-    
-elif player1_score < player2_score:
-    text = pygame.font.render("asddasd",True, "white")
-    screen.blit(text, (250,250))
-    
-pygame.display.flip()
-pygame.time.wait(5000)
+    for i in range(10):
+        x1 = random.randrange(screen_width)
+        y1 = random.randrange(screen_height)
+        x2 = random.randrange(screen_width)
+        y2 = random.randrange(screen_height)
 
-# while True:
-#     for event in pygame.event.get():
-#         print(event)
-    
-#     for i in range(10):
-#         x1 = random.randrange(screen_width)
-#         y1 = random.randrange(screen_height)
-#         x2 = random.randrange(screen_width)
-#         y2 = random.randrange(screen_height)
+        distance_from_center1 = math.hypot(x1-250, y1-250)
+        distance_from_center2 = math.hypot(x2-250, y2-250)
+        is_in_circle1 = distance_from_center1 <= screen_width/2
+        is_in_circle2 = distance_from_center2 <= screen_width/2
 
-#         distance_from_center1 = math.hypot(x1-250, y1-250)
-#         distance_from_center2 = math.hypot(x2-250, y2-250)
-#         is_in_circle1 = distance_from_center1 <= screen_width/2
-#         is_in_circle2 = distance_from_center2 <= screen_width/2
-
-#         if is_in_circle1 == True:
-#             pygame.draw.circle(screen, "yellow", (x1,y1),5)
-#             pygame.display.flip()
-#             player1_score = player1_score + 1
-#         else:
-#             pygame.draw.circle(screen, "blue", (x1,y1),5)
-#             pygame.display.flip()
-#         if is_in_circle2 == True:
-#             pygame.draw.circle(screen, "pink", (x2,y2),5)
-#             pygame.display.flip()
-#             pygame.time.wait(500)
-#             player2_score = player2_score + 1
-#         else:
-#             pygame.draw.circle(screen, "purple", (x2,y2),5)
-#             pygame.display.flip()
-#             pygame.time.wait(500)
+        if is_in_circle1 == True:
+            pygame.draw.circle(screen, "yellow", (x1,y1),5)
+            pygame.display.flip()
+            player1_score = player1_score + 1
+        else:
+            pygame.draw.circle(screen, "blue", (x1,y1),5)
+            pygame.display.flip()
+        if is_in_circle2 == True:
+            pygame.draw.circle(screen, "pink", (x2,y2),5)
+            pygame.display.flip()
+            pygame.time.wait(500)
+            player2_score = player2_score + 1
+        else:
+            pygame.draw.circle(screen, "purple", (x2,y2),5)
+            pygame.display.flip()
+            pygame.time.wait(500)
         
-#     if player1_score == player2_score:
-#         text = pygame.font.render("asdasda",True, "white")
-#         screen.blit(text, (250,250))
+    if player1_score == player2_score:
+        font = pygame.font.Font(None, 48)
+        text = font.render("The players tied!", True, "white")
+        screen.blit(text, (250, 250))
         
-#     elif player1_score > player2_score:
-#         text = pygame.font.render("adsasd",True, "white")
-#         screen.blit(text, (250,250))
+    elif player1_score > player2_score:
+        font = pygame.font.Font(None, 48)
+        text = font.render("Player 1 wins!", True, "white")
+        screen.blit(text, (250, 250))
         
-#     elif player1_score < player2_score:
-#         text = pygame.font.render("asddasd",True, "white")
-#         screen.blit(text, (250,250))
-        
-#     pygame.display.flip()
-#     pygame.time.wait(5000)
+    elif player1_score < player2_score:
+        font = pygame.font.Font(None, 48)
+        text = font.render("Player 2 wins!", True, "white")
+        screen.blit(text, (100, 250))
+    
+    pygame.display.flip()
+    pygame.time.wait(5000)
+    
+    break
