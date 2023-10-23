0d46208fc2a239c3996208f4d76a67de441bdab5
Anthony <anthonyp1455@gmail.com>
2023-10-23 16:54:58 -0400

    pyramid

--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -1,11 +1,11 @@
 
 
-def star_pyramid():
-    for i in range(amount):
-        print("*"*(amount - i))
-        
-        
 def star_pyramidr():
+    for i in range(amount, 0 , -1):
+        print("*"*(i))
+        
+        
+def star_pyramid():
     for i in range(amount+1):
         print("*"*(i))
 
