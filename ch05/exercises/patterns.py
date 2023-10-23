36658eabaa6cc6d925ad64d2599cde1c2fd04662
Anthony <anthonyp1455@gmail.com>
2023-10-23 16:50:29 -0400

    pyramid

--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -1,17 +1,15 @@
 
 
 def star_pyramid():
-    star = "*"
     for i in range(amount):
-        print(star*(amount - i))
+        print("*"*(amount - i))
         
         
 def star_pyramidr():
-    star = "*"
     for i in range(amount+1):
-        print(star*(i))
+        print("*"*(i))
 
 amount = int(input("pleases enter the amount of stars you want to print "))
 
-star_pyramid()
 star_pyramidr()
+star_pyramid()
